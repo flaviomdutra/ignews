489b64e25a6f42ecbe66c42d48aa7d5f9fc5ed68
Flávio Dutra <flaviomdutra@outlook.com>
2023-03-02 23:47:18 -0300

    feat: preview posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,25 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import Link from "next/link";
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
+
+import { getPrismicClient } from "src/services/prismic";
+
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -10,38 +28,48 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="">
-            <time>12 de marco de 2021</time>
-            <strong>Creating a Monorepo with Lerna</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aut
-              voluptates distinctio explicabo dicta est nihil enim, maiores
-              rerum obcaecati eaque consequatur fuga tempora repellendus
-              exercitationem quas ratione! Praesentium, minus.
-            </p>
-          </a>
-          <a href="">
-            <time>12 de marco de 2021</time>
-            <strong>Creating a Monorepo with Lerna</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aut
-              voluptates distinctio explicabo dicta est nihil enim, maiores
-              rerum obcaecati eaque consequatur fuga tempora repellendus
-              exercitationem quas ratione! Praesentium, minus.
-            </p>
-          </a>
-          <a href="">
-            <time>12 de marco de 2021</time>
-            <strong>Creating a Monorepo with Lerna</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aut
-              voluptates distinctio explicabo dicta est nihil enim, maiores
-              rerum obcaecati eaque consequatur fuga tempora repellendus
-              exercitationem quas ratione! Praesentium, minus.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <Link key={post.slug} href={`/posts/${post.slug}`} legacyBehavior>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "post")],
+    { fetch: ["post.title", "post.content"], pageSize: 100 }
+  );
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
+
+  return {
+    props: { posts },
+  };
+};
